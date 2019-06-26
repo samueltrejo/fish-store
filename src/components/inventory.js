d23fe4fe55c344f62674503ab9b9cdc2fc5045cc
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-06-25 20:09:49 -0500

    pull data from firebase

--- src/components/inventory.js
+++ src/components/inventory.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
+import fishData from '../helpers/data/fish-data';
+
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(error => console.error(error));
+  }
+
   render() {
     return (
       <div className="Inventory col-4">
